ad7f95c410cc85fb927aea2d43914cf399eb9d17
Ryu Mitsurugi <Pittyparty1023@gmail.com>
2021-05-31 18:22:07 -0400

    Subiendo parte del proyecto

--- index.js
+++ index.js
@@ -1,32 +1,87 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const hbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require ('nodemailer');
+require('dotenv').config();
+
+//Nodemailer//
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.MAIL_USER, // generated ethereal user
+      pass: process.env.MAIL_PASS, // generated ethereal password
+    },
+  });
+  transporter.verify().then(()=>{
+    console.log("Listo para enviar correo!");
+});
+
+/*Settings*/ 
+app.set("view engine", ".hbs");
+app.set('Views', path.join(__dirname, 'Views'))
+app.set(express.static(path.join(__dirname, "Public")))
+app.use(express.urlencoded({
+    extended: false
+}));
+
+/*Handlebars config*/ 
+app.engine('.hbs', hbs({
+    defaultLayout: "main",
+    LayoutsDir: path.join(app.get('Views'), 'Layouts'),
+    PartialsDir: path.join(app.get('Views'), 'Partials'),
+    extname: ".hbs"
+
+}))
 
 app.get('/', (req, res)=>{
-    res.send('Ruta de inicio de nuestro proyecto');
-})
-
-app.get('/Home', (req, res)=>{
-    res.send('Pagina de Home');
+    res.render('Home', 
+    {ruta: "/Assets/css/Home.css"});
 })
 
 app.get('/AboutUs', (req, res)=>{
-    res.send('Pagina de informacion de empresa');
+    res.render('AboutUs', 
+    {ruta: "/Assets/css/AboutUs.css"});
 })
 
 app.get('/Especialties', (req, res)=>{
-    res.send('Pagina de especialidades');
+    res.render('Especialties', 
+    {ruta: "/Assets/css/Especialties.css"});
 })
 
 app.get('/Form', (req, res)=>{
-    res.send('Pagina de formulario');
+    res.render('Form', 
+    {ruta: "/Assets/css/Form.css"});
 })
 
 app.get('/404', (req, res)=>{
-    res.send('Pagina de  error 404');
+    res.render('404', 
+    {ruta: "/Assets/css/404.css"});
 })
 
+app. post ('/Form', async(req, res)=>{
+    // send mail with defined transport object
+   await transporter.sendMail({
+   from: process.env.MAIL_USER, // sender address
+   to:process.env.MAIL_USER, // list of receivers
+   subject: `${req.body.nombre} Requiere de su atención sobre ${req.body.asunto}`, // Subject line
+   html: `<h1>Nombre:${req.body.nombre}</h1>
+       <h1>Correo:${req.body.email}</h1>
+       <h1>Solicita la siguiente información:</h1>
+   <h1>${req.body.mensaje}</h1>` // html body
+ });
+   res.redirect('/');
+})
+
+app.use((req, res)=>{
+    res.render('404');
+});
+
+const PORT = process.env.PORT || 4000;
 app.listen (PORT, ()=>{
-    console.log (`Server on http://localhost:${PORT}`);
+    console.log(app.get('Views'));
+    console.log (`Server at http://localhost:${PORT}`);
 })
